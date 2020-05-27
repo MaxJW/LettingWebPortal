4809a13986ec4baa49c2e4f79ad91178a7ddb0c6
MaxJW <maxjw98@gmail.com>
2020-05-27 14:47:53 +0100

    Added basic property class
    Followed a few ASP.NET tutorials from the documentation, and created a simple sql database with a property class.
    
    These have CRUD automatically created.
    
    Looking to create a customer portal to be able to select these and add them to a personal list.

--- LettingWeb/Startup.cs
+++ LettingWeb/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using LettingWeb.Data;
 
 namespace LettingWeb
 {
@@ -24,6 +26,9 @@ namespace LettingWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<LettingWebContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("LettingWebContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
